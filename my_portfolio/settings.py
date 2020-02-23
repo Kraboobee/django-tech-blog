4e81615e7a478b7628a507f54ba9c16aeb895b4c
Kaamraan <kraboobee@gmail.com>
2020-02-24 00:56:51 +0200

    Changed secret key

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+from secrets import *
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jc&z=4y-tx&zq$481&es)xp@43t0)kei-j51!_!w%6u8*81+jg'
+SECRET_KEY = sk
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
