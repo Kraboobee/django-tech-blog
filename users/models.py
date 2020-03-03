65c7f7bdfe768e3cc0bf7fabb33bb868f2f894d1
Kaamraan <kraboobee@gmail.com>
2020-03-03 14:43:30 +0200

    Added profile functionality. Profile pictures can be added, and user profiles are created as soon as a user has registered.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Profile model for users
+	# Provides a one-to-one relationship with Users. 
+	# If a user is deleted, their profile will be deleted as well
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+# To-string method for Profile
+	def __str__(self):
+		return f'{self.user.username} Profile'
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 # Form for user registration
@@ -22,3 +23,7 @@ def register(request):
 		form = UserRegisterForm()
 	return render(request, 'users/register.html', {'form': form})
 
+# This ensures that a user is not able to access the profile page unless they are logged in
+@login_required
+def profile(request):
+	return render(request, 'users/profile.html')
