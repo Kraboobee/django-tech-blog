da61944486ea3dc72a7e6df1879843b833d6fdec
Kaamraan <kraboobee@gmail.com>
2020-03-05 10:55:48 +0200

    Added forms to update account info and profile picture. Posts also now show author's profile picture.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 # Form for user registration
 
@@ -26,4 +26,24 @@ def register(request):
 # This ensures that a user is not able to access the profile page unless they are logged in
 @login_required
 def profile(request):
-	return render(request, 'users/profile.html')
+	if request.method == 'POST':
+		u_form = UserUpdateForm(request.POST, instance = request.user)
+		p_form = ProfileUpdateForm(request.POST, 
+								   request.FILES, 
+								   instance = request.user.profile)
+		if u_form.is_valid() and p_form.is_valid():
+			u_form.save()
+			p_form.save()
+			messages.success(request, f'Your account has been updated! You are now able to log in')
+			return redirect('profile')
+
+	else:
+		u_form = UserUpdateForm(instance = request.user)
+		p_form = ProfileUpdateForm(instance = request.user.profile)
+
+	context = {
+		'u_form': u_form,
+		'p_form': p_form
+	}
+
+	return render(request, 'users/profile.html', context)
