0f1ba4ce8cdfa55e859bc4de12f7b4f25b29e960
Kaamraan Raboobee <kraboobee@pop-os.localdomain>
2020-03-21 21:22:09 +0200

    Added functionality to see all posts by user. Added pagination to user's list of posts.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.mixins import (
 	LoginRequiredMixin,
 	UserPassesTestMixin
 )
+from django.contrib.auth.models import User
 from django.views.generic import (
 	CreateView, 
 	DeleteView,
@@ -25,11 +26,24 @@ class PostListView(ListView):
 	model = Post
 	template_name = 'blog/home.html'
 	context_object_name = 'posts'
+	paginate_by = 5
+
+
+class UserPostListView(ListView):
+	model = Post
+	template_name = 'blog/user_posts.html'
+	context_object_name = 'posts'
 	ordering = ['-date_posted']
 	paginate_by = 5
 
+	def get_queryset(self):
+		user = get_object_or_404(User, username=self.kwargs.get('username'))
+		return Post.objects.filter(author=user).order_by('-date_posted')
+
+
 class PostDetailView(DetailView):
 	model = Post
+
 
 class PostCreateView(LoginRequiredMixin, CreateView):
 	model = Post
