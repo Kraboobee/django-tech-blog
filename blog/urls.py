1f383f38e2cc259916f91a2a6d5c0cf84cb0c6ab
Kraboobee <kraboobee@gmail.com>
2020-03-26 23:48:24 +0200

    Added search functionality for content and titles of posts

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from .views import (
 	PostDeleteView, 
 	PostListView,
 	PostUpdateView,
+    SearchResultListView,
     UserPostListView
 )
 from . import views
@@ -13,6 +14,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('search/<str:title>', SearchResultListView.as_view(), name='search-results'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
