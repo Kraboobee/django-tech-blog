f55d2218d913f5fd6a24ecc78e40080fac0e84b1
Kaamraan <kraboobee@gmail.com>
2020-04-05 18:44:35 +0200

    fixed an error in user/models.py

--- users/models.py
+++ users/models.py
@@ -1,20 +1,20 @@
-from django.db import models
+from django.db 					import models
 from django.contrib.auth.models import User
-from PIL import Image
+from PIL 						import Image
 
 # Profile model for users
 	# Provides a one-to-one relationship with Users. 
 	# If a user is deleted, their profile will be deleted as well
 class Profile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+	user 	= models.OneToOneField(User, on_delete=models.CASCADE)
+	image 	= models.ImageField(default='default.jpg', upload_to='profile_pics')
 
 # To-string method for Profile
 	def __str__(self):
 		return f'{self.user.username} Profile'
 
-	def save(self):
-		super().save()
+	def save(self, *args, **kwargs):
+		super().save(*args, **kwargs)
 
 		img = Image.open(self.image.path)
 
